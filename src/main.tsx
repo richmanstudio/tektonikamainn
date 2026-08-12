8d43c95ae76fd322124c23f3bd5d05b28e278523
Richman <133752573+richmanstudio@users.noreply.github.com>
2026-08-12 21:04:42 +1000

    seo: serve language variants from dedicated URL prefixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/index";
-import { LanguageProvider } from "./i18n";
+import { LanguageProvider, type Lang } from "./i18n";
 import { CmsProvider } from "./cms";
+import LanguageUrlSync from "./components/LanguageUrlSync";
 import SeoManager from "./components/SeoManager";
 import "./index.css";
 
+const localeMatch = window.location.pathname.match(/^\/(en|zh)(?=\/|$)/);
+const initialLang: Lang = localeMatch?.[1] === "en" ? "en" : localeMatch?.[1] === "zh" ? "zh" : "ru";
+const basename = initialLang === "ru" ? "/" : `/${initialLang}`;
+
+// URL is the source of truth for language on direct visits and search-engine crawls.
+localStorage.setItem("tektonika-lang", initialLang);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <CmsProvider>
-        <BrowserRouter>
+    <BrowserRouter basename={basename}>
+      <LanguageProvider>
+        <CmsProvider>
+          <LanguageUrlSync />
           <SeoManager />
           <AppRoutes />
-        </BrowserRouter>
-      </CmsProvider>
-    </LanguageProvider>
+        </CmsProvider>
+      </LanguageProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
